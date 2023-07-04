ec2c51c94d022f5a1aa1f516d3d837c3a8f78e0d
Glitch (glitch-blank-website) <none>
2023-07-04 04:13:20 +0000

    🛋📇 Checkpoint ./script.js:49897981/175 ./index.html:49897981/338

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-undef */ // Ignore this line, it just stops the editor from complaining.
 // Hello!!!! Welcome to Kaboom!
 
 // This line imports the Kaboom library.
-import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
+import kaboom from "https://unpkg.com/kaboom@^3000.0.12/dist/kaboom.mjs";
 
 // Now, let's initialize it. This line just gives you a fullscreen canvas.
 kaboom();
 
 // Add some text. The add() command takes in arguments in a list. You can think of each of these as a "property" of the object.
+
+add([
+    text("hello, world"),
+    pos(120, 80),
+]);
 
