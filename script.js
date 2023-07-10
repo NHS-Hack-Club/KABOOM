e3c18f4792a06ae444b83a857230262516c512e8
Glitch (glitch-blank-website) <none>
2023-07-10 22:35:57 +0000

    🖼🏜 Checkpoint ./script.js:49897981/17656

--- script.js
+++ script.js
@@ -9,18 +9,87 @@ kaboom();
 
 // Load in a sprite. I've included a few in the project (check the Assets tab to the left). To get a sprite's URL, click it in the Assets view then click "Copy URL".
 loadSprite("bean", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/bean.png?v=1688618964513")
+loadSprite("coin", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/coin.png?v=1688618966800")
+loadSprite("spike", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/spike.png?v=1688618977354")
+loadSprite("grass", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/grass.png?v=1688618971014")
+loadSprite("ghosty", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/ghosty.png?v=1688618969880")
+
 
 
 // Add some text. The add() command adds what's called a "Game Object". It takes in a list (in [square brackets]) of "components", or properties of the object.
 add([
     text("hello, world"),
-    pos(120, 80),
-]);
+  ])
 
-// Now let's add a player character. We store this one in a variable, so we can refer to it later.
-const player = add([
-	sprite("bean"),   // sprite() component makes it render as a sprite
-	pos(120, 160),     // pos() component gives it position, also enables movement
-	rotate(0),        // rotate() component gives it rotation
-	anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
-])
+
+const SPEED = 480
+
+setGravity(2400)
+
+const level = addLevel([
+	// Design the level layout with symbols
+	"@  ^ $$",
+	"=======",
+], {
+	// The size of each grid
+	tileWidth: 64,
+	tileHeight: 64,
+	// The position of the top left block
+	pos: vec2(100, 200),
+	// Define what each symbol means (in components)
+	tiles: {
+		"@": () => [
+			sprite("bean"),
+			area(),
+			body(),
+			anchor("bot"),
+			"player",
+		],
+		"=": () => [
+			sprite("grass"),
+			area(),
+			body({ isStatic: true }),
+			anchor("bot"),
+		],
+		"$": () => [
+			sprite("coin"),
+			area(),
+			anchor("bot"),
+			"coin",
+		],
+		"^": () => [
+			sprite("spike"),
+			area(),
+			anchor("bot"),
+			"danger",
+		],
+	},
+})
+
+// Get the player object from tag
+const player = level.get("player")[0]
+
+// Movements
+onKeyPress("space", () => {
+	if (player.isGrounded()) {
+		player.jump()
+	}
+})
+
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+
+// Back to the original position if hit a "danger" item
+player.onCollide("danger", () => {
+	player.pos = level.tile2Pos(0, 0)
+})
+
+// Eat the coin!
+player.onCollide("coin", (coin) => {
+	destroy(coin)
+})
