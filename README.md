678a79abe06bf97ff9e6cbf76414a1871b37db31
Glitch (kaboom-game-template) <none>
2023-12-08 22:59:46 +0000

    🏦🎋 Checkpoint ./README.md:28851483/210 ./script.js:28851483/3831 ./index.html:28851483/112

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Welcome to Kaboom!
+# Welcome to the NHS Kaboom Project!
 
 Click `script.js` in the sidebar over there ↙️ to get started!
+You shouldn't have to change anything else, only `script.js`
+
+Happy coding!
--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-/* eslint-disable no-undef */ // Ignore this line, it just stops the editor from complaining.
-
-
-/*
- ______    _______  _______  ______     _______  __   __  ___   _______ 
-|    _ |  |       ||   _   ||      |   |       ||  | |  ||   | |       |
-|   | ||  |    ___||  |_|  ||  _    |  |_     _||  |_|  ||   | |  _____|
-|   |_||_ |   |___ |       || | |   |    |   |  |       ||   | | |_____ 
-|    __  ||    ___||       || |_|   |    |   |  |       ||   | |_____  |
-|   |  | ||   |___ |   _   ||       |    |   |  |   _   ||   |  _____| |
-|___|  |_||_______||__| |__||______|     |___|  |__| |__||___| |_______|
-
-Hello!!!! Welcome to Kaboom! In order to make this game work, you'll need to make a few changes. Look below to see where you'll need to add some code!
-
-If you want to refer to some documentation about making games this way, check out https://kaboomjs.com/. The examples (click "Playground" in the sidebar) might also be helpful.
-
-1. Right now, it's pretty hard for Bean to jump over the spikes. Can you make Bean float a bit more?
-2. Right now, the coins just sit there when Bean hits them. Bean should be able to collect them and get rich. How can you make the coins disappear?
-3. This level's looking a bit small and cramped. Why don't you make it wider, to give Bean some room to breathe?
-  3b. Try adding some more variety in the objects (hint: check the Assets tab to the left for a bunch of sprites I've left you!)
-4. Add some text to the screen to explain the controls.
-*/
-
 // This line imports the Kaboom library.
 import kaboom from "https://unpkg.com/kaboom@^3000.0.12/dist/kaboom.mjs";
 
-// Now, let's initialize it. This line just gives you a fullscreen canvas.
+// This creates the kaboom game, makes it full screen
 kaboom({
-  background: [135, 206, 235],
+  background: [135, 206, 235], // sets the color of the game
 });
 
-// Load in a sprite. I've included a bunch in the project (check the Assets tab to the left). To get a sprite's URL, click it in the Assets view then click "Copy URL".
+// These are the sprites for the game (ignore the errors )
 loadSprite("bean", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/bean.png?v=1688618964513")
 loadSprite("coin", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/coin.png?v=1688618966800")
 loadSprite("spike", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/spike.png?v=1688618977354")
@@ -43,16 +20,29 @@ const SPEED = 480
 
 
 // Set the game's gravity.
-setGravity(5000)
+setGravity(1250)
 
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+// EDIT THE LEVELS HERE!!!!!!!//////////////////////////////////////////////////////
 const levels = [
   [
-	// Design the level layout with symbols
-	"@  ^ $$",
+  "@      ",
+	"   ^ $$",
 	"=======",
   ],
-  // TODO add more levels
+  [
+  "@          ",
+  "    ^^^ $$$",
+  "============"
+  ]
 ]
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
 var levelID = 0;
 var level;
@@ -63,6 +53,7 @@ function renderNewLevel(id) {
   if (level !== undefined) {
     level.destroy()
   }
+  
   level = addLevel(
   levels[id], {
 	// The size of each grid tile
@@ -76,7 +67,7 @@ function renderNewLevel(id) {
 			sprite("bean"), 
 			area(), 
 			body(),
-			// anchor("bot"), // This one just sets where the "base" position for rendering is.
+			anchor("bot"), 
 			"player", // Including a string here adds a tag to the object that you can refer to later.
 		],
 		"=": () => [
@@ -101,10 +92,9 @@ function renderNewLevel(id) {
 })
   
   player = level.get("player")[0]
-      initInteractions(player)
 }
 
-renderNewLevel(levelID)
+renderNewLevel(levelID) // Starts up the first level
 
   
 // Movements
@@ -122,37 +112,25 @@ onKeyDown("right", () => {
 	player.move(SPEED, 0)
 })
 
-// Back to the original position if hit a "danger" item
-player.onCollide("danger", () => {
-	player.pos = level.tile2Pos(0, 0)
-})
-
-
-player.onCollide("coin", (coin) => {
-
-})
-
-function initInteractions(pl) {
   
 // Back to the original position if hit a "danger" item
 player.onCollide("danger", () => {
 
   addKaboom(vec2(width() / 2, height() / 2), {scale: 2}) // Show an explosion!
   
-	player.pos = level.tile2Pos(0, 0)
+	player.pos = level.tile2Pos(0, -2);
 
 })
 
 
 player.onCollide("coin", (theCoin) => {
-  console.log("boop")
   destroy(theCoin)
 })
 
 player.onCollide("portal", () => {
   level.destroy()
-  renderNewLevel(1)
+  renderNewLevel(++levelID)
 })
-}
+
 
 
