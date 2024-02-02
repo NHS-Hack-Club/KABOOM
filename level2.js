ef668714e86e6b0e1e9f8528dc1d7a3fb901d22a
Glitch (nhs-kaboom) <none>
2024-02-02 22:10:14 +0000

    🌝🌴 Updated with Glitch

--- level2.js
+++ level2.js
@@ -1,82 +1,137 @@
-class Level2 extends Level {
+// Made by Justin
+window.Level = class {
+	constructor() {
+	  this.MAXX = 1000
+	  this.MAXY = 1000
+	  this.levelLayout=[
+		"@      ",
+		"$$$$$$$$$$$#",
+		"============",
+	  ]
+	  // Set the game's gravity.
+	  setGravity(1250)
+	}
+	die() {
+		destroy(this.player);
+		document.dispatchEvent(new CustomEvent("die"));
+	}
+	
+	setControls() {
+		var player = this.player
+		// Movement
+		onKeyDown("left", () => {
+			player.move(-480, 0)
+		})
+		
+		onKeyDown("right", () => {
+			player.move(480, 0)
+		})
+		
+		onKeyPress("space", () => {
+			player.doubleJump();
+		})
+		
+		onKeyPress("up", () => {
+			player.doubleJump();
+		})
+		
+		onKeyPress("r", () => {
+			player.pos.y  = this.MAXY+1;
+			player.pos.x = this.MAXX+1;
+		});
+		
+		player.onDoubleJump(() => {
+			addKaboom(camPos().sub(0,50), {scale: 0.5});
+		})
+		
+		player.onPhysicsResolve(() => {
+			// Set the viewport center to player.pos
+			camPos(player.worldPos())
+		})
+		
+		player.onUpdate(() => {
+			// Set the viewport center to player.pos
+			camPos(player.worldPos())
+			// Prevent player from going off
+			if (player.pos.y >= this.MAXY || player.pos.y <= -this.MAXY || player.pos.x >= this.MAXX || player.pos.x <=-this.MAXX) {
+				this.die();
+			}
+		})
+	}
+	
+	renderNewLevel() {
+	  this.level = addLevel(
+	  this.levelLayout, {
+		// The size of each grid tile
+		tileWidth: 64,
+		tileHeight: 64,
+		// The on-screen position of the top left block
+		pos: vec2(100, 400),
+		// Define what each symbol means. Each symbol has a "game object" associated with it.
+		tiles: {
+		  "@": () => [
+			sprite("bean"), 
+			area(), 
+			body(),
+			anchor("bot"), 
+			doubleJump(10),
+			"player", // Including a string here adds a tag to the object that you can refer to later.
+		  ],
+		  "=": () => [
+			sprite("grass"),
+			area(),
+			body({ isStatic: true }),
+			anchor("bot"),
+		  ],
+		  "$": () => [
+			sprite("coin"),
+			area(),
+			anchor("bot"),
+			"coin",
+		  ],
+		  "^": () => [
+			sprite("spike"),
+			area(),
+			anchor("bot"),
+			"danger",
+		  ],
+		  "#": () => [
+			sprite("portal"),
+			area(),
+			anchor("bot"),
+			"portal"
+		  ]
+		},
+	  })
   
-  constructor(global) {
-    super()
-    this.global = global
-    this.MAXX = 1000
-    this.MAXY = 1000
-    this.levelLayout=[
-      "@      ",
-      "$$$$$$$ $$$ #",
-      "=============",
-    ]
-    // Set the game's gravity.
-    setGravity(1250)
-  }
+	  this.player = this.level.get("player")[0]
+	  this.setControls()
+	  this.initializeInteractions();
+	}
+	
+	initializeInteractions() {
+	  var player = this.player
+	  var level = this.level
   
-  renderNewLevel() {
-    this.level = addLevel(
-    this.levelLayout, {
-      // The size of each grid tile
-      tileWidth: 64,
-      tileHeight: 64,
-      // The on-screen position of the top left block
-      pos: vec2(100, 400),
-      // Define what each symbol means. Each symbol has a "game object" associated with it.
-      tiles: {
-        "@": () => [
-          sprite("bean"), 
-          area(), 
-          body(),
-          anchor("bot"), 
-          doubleJump(2),
-          "player", // Including a string here adds a tag to the object that you can refer to later.
-        ],
-        "=": () => [
-          sprite("grass"),
-          area(),
-          body({ isStatic: true }),
-          anchor("bot"),
-        ],
-        "$": () => [
-          sprite("coin"),
-          area(),
-          anchor("bot"),
-          "coin",
-        ],
-        "^": () => [
-          sprite("spike"),
-          area(),
-          anchor("bot"),
-          "danger",
-        ],
-        "#": () => [
-          sprite("portal"),
-          area(),
-          anchor("bot"),
-          "portal"
-        ]
-      },
-    })
-
-    this.player = this.level.get("player")[0]
-    this.setControls();
-    this.initializeInteractions();
-  }
+	  // Back to the original position if hit a "danger" item
+	  player.onCollide("danger", () => {
+		this.die()
+	  })
   
-  initializeInteractions() {
-    var player = this.player
-    var level = this.level
-
-
-    // Back to the original position if hit a "danger" item
-    player.onCollide("danger", () => {
-      this.die()
-    })
-
-    player.onCollide("coin", (theCoin) => {
-      destroy(theCoin)
-    })
+	  player.onCollide("coin", (theCoin) => {
+		destroy(theCoin)
+	  })
+	  player.onCollide("portal", () => {
+		level.destroy()
   
-  }
+		document.dispatchEvent(new CustomEvent("nextLevel"));
+	  })
+	}
 }
+
+/* 
+This creates a new instance of the level
+DO NOT MODIFY
+*/
+window.l = new window.Level();
+window.l.renderNewLevel();
--- script.js
+++ script.js
@@ -1,76 +1,129 @@
-// This line imports the Kaboom library.
-import kaboom from "https://unpkg.com/kaboom@^3000.0.12/dist/kaboom.mjs";
+/*
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////DO NOT MODIFY THE CODE BELOW////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+*/
+function loadjscssfile(filename, filetype){
+	if (filetype=="js"){ //if filename is a external JavaScript file
+		var fileref=document.createElement('script')
+		fileref.setAttribute("type","text/javascript")
+		fileref.setAttribute("src", filename)
+	}
+	else if (filetype=="css"){ //if filename is an external CSS file
+		var fileref=document.createElement("link")
+		fileref.setAttribute("rel", "stylesheet")
+		fileref.setAttribute("type", "text/css")
+		fileref.setAttribute("href", filename)
+	}
+	if (typeof fileref!="undefined")
+		document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+function removejscssfile(filename, filetype){
+	var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+	var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+	var allsuspects=document.getElementsByTagName(targetelement)
+	for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+		if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+		allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+	}
+}  
+/*
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// YOU CAN MODIFY CODE BELOW /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+/*
+This function loads the images for the sprites
+*/
+function loadSprites() {
+	loadSprite("bean", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/bean.png?v=1688618964513")
+	loadSprite("coin", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/coin.png?v=1688618966800")
+	loadSprite("spike", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/spike.png?v=1688618977354")
+	loadSprite("grass", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/grass.png?v=1688618971014")
+	loadSprite("ghosty", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/ghosty.png?v=1688618969880")
+	loadSprite("portal", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/portal.png?v=1688618976168")
+	loadSprite("ninja", "https://cdn.glitch.global/e4191fbe-6aa2-4565-b2f8-45b16c7137fd/ninja.png?v=1705703717150")
+	loadSprite("enemy", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/onion.png?v=1688618975053")
+	loadSprite("diya", "https://cdn.glitch.global/e4191fbe-6aa2-4565-b2f8-45b16c7137fd/diya.png?v=1706308399460")
+}
+
+
 
 
 /* 
-All global variables
+Global variables
 Coins: For shop
-Lives: Loose each time they die  
+Lives: Lose each time they die  
 */
-var global = {coins:0, lives:3}
+window.global = {coins:0, lives:3}
 
+/* 
+Level Management
+*/
+var levelID = 0
+const levels = [
+	"level1.js",
+	"level2.js",
+]
 
-
-  //Checks to make sure starting level is within bounds
-  // var trueLevelNum = false;
-  
-//   while (trueLevelNum == false){
-//     
-//   }
-  
-    
-var levelID, levels=[];
-document.getElementById("startButton").addEventListener("click", () => {
-  //set starting level index if true
-  kaboom({
-    background: [135, 206, 235], // sets the color of the game
-
-  });
-  // These are the sprites for the game (ignore the errors)
-  loadSprite("bean", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/bean.png?v=1688618964513")
-  loadSprite("coin", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/coin.png?v=1688618966800")
-  loadSprite("spike", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/spike.png?v=1688618977354")
-  loadSprite("grass", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/grass.png?v=1688618971014")
-  loadSprite("ghosty", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/ghosty.png?v=1688618969880")
-  loadSprite("portal", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/portal.png?v=1688618976168")
-  loadSprite("ninja", "https://cdn.glitch.global/e4191fbe-6aa2-4565-b2f8-45b16c7137fd/ninja.png?v=1705703717150")
-  loadSprite("enemy", "https://cdn.glitch.global/6e7edbfb-3679-4519-bb57-df3008b83592/onion.png?v=1688618975053")
-  loadSprite("diya", "https://cdn.glitch.global/e4191fbe-6aa2-4565-b2f8-45b16c7137fd/diya.png?v=1706308399460")
-  levels = [
-    new Level1(global),
-    new Level2(global),
-    new Level3(global),
-    new Level4(global),
-    new Level5(global),
-    new Level6(global),
-    new Level7(global),
-    new Level8(global),
-    new Level9(global),
-    new Level10(global),
-    new Level11(global),
-  ]
-  if (parseInt(document.getElementById("playerLevelChoice").value) <= levels.length){
-    levelID = parseInt(document.getElementById("playerLevelChoice").value);
-  }      
-  console.log(levelID)
-  //Renders the first level with level Id
-  levels[levelID].renderNewLevel(); 
+/*
+Handler for starting the game
+*/
+document.getElementById("selection").addEventListener("submit", (e) => {
+	e.preventDefault(); // prevents the page from being reloaded (VERY IMPORTANT DO NOT DELETE)
+	// Checks if the levelID inputted is valid
+	var playerChoice = document.getElementById("playerLevelChoice").value
+	if (parseInt(playerChoice) <= levels.length || playerChoice == ""){
+		if (playerChoice == "") levelID = 0
+		else levelID = parseInt(playerChoice)
+		
+		// Creates the game
+		kaboom({
+			background: [135, 206, 235], // sets the color of the game
+		});
+		// Load the sprites
+		loadSprites();
+		
+		//Renders the first level with level id
+		console.log("Level: " + levelID)
+		loadjscssfile(levels[levelID], "js");
+	} else {
+		alert("Not a level! (Remember that the levels are 0-indexed)")
+		return;
+	}
 }) 
 
-  
-  
+/*
+Handler for going to the next level 
+DO NOT CHANGE THIS CODE
+*/
 document.addEventListener("nextLevel", () => {
-    if (levelID+1 >= levels.length) {
-      alert("No more levels!")
-    } else {
-      levels[++levelID].renderNewLevel();
-    }
+	if (levelID+1 >= levels.length) {
+		alert("No more levels!")
+	} else {
+		delete window.Level;
+		delete window.l;
+		removejscssfile(levels[levelID], "js");
+		loadjscssfile(levels[++levelID], "js");
+		console.log("Level: " + levelID)
+	}
 })
 
-
+/*
+Handler for restarting a level
+*/
 document.addEventListener("die", () => {
-  
-      levels[levelID].renderNewLevel();
+	window.l.renderNewLevel();
 })
 
 
